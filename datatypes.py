93513c7290201913433cf73aee1ca79eb61392ea
Helen Okereke <okerekehelenugoeze@gmail.com>
2023-10-05 04:32:49 +0100

    update

---
+++ datatypes.py
@@ -0,0 +1,39 @@
+#integer variables
+age = 25
+score = 90
+
+#float variables
+average = 65.7
+pie = 3.142
+
+#complex variables
+com1 = 3.14j
+
+#string Variables
+school = "University of Calabar" 
+state = "Rivers"
+firstname = "Patrick"
+
+#Boolean Variables
+is_rich = True
+president_t = False
+is_tall = False
+submitted = True
+
+#list in Python
+values = ["23", 50, "Helen", "Grace", True]
+states = ["Abia", "Enugu", "Ebonyi", "Anambara", "Imo"]
+
+print(states[0])
+print(type(pie))
+print(type(values[1]))
+
+
+
+
+
+
+
+
+
+
