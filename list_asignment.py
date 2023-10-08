7e378a304662d7f060bc4470480027dd26acffcb
Helen Okereke <okerekehelenugoeze@gmail.com>
2023-10-08 19:52:07 +0100

    asignment on list

---
+++ list_asignment.py
@@ -0,0 +1,10 @@
+
+teams = {"Alpha":["Helen", "Grace", "Favour"],
+         "Beta":["John", "Solomon", "Mary", "Jerome"],
+         "Brainy":["cowell", "mathilda", "Graceline"], 
+         "Havard":["Diamond", "Mark", "Raycon"],
+         "Stars":["Morit", "Sandra", "Henry"]}
+
+#printing all the members of the harvard and Beta teams
+print(teams["Havard"][1])
+presidents = {"2010":"Goodluck Jonathan"}
