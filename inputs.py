c232af10d563ceb18336991374f2fbb712b693dd
Helen Okereke <okerekehelenugoeze@gmail.com>
2023-10-13 00:14:09 +0100

    update

---
+++ inputs.py
@@ -0,0 +1,8 @@
+
+
+
+print("\n\n\t WELCOME TO OUR INPUT APP")
+name = input("Please Enter your Name: ")
+age = input("How old are you "+name+": ")
+
+print("\nHello",name,", You are", age,"years old. Thank you for registering on our App ")
