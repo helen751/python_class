723153e5feeecb3e3fb6036287850e63a759fd60
Helen Okereke <okerekehelenugoeze@gmail.com>
2023-12-07 21:07:15 +0100

    functions

---
+++ function.py
@@ -0,0 +1,23 @@
+numbers = [0,0,0,0,0]
+for i in range(5):
+    num = input("Enter Number ("+str(i) + "): ")
+    numbers[i] = num
+print(numbers)
+
+name = input("What is your name: ")
+def welcome():
+    print("Hello", name, "Welcome to our App")
+
+welcome()
+
+
+a = 5.6
+def add(a, b, c=12):
+    #local variables
+    result = a + b + c
+    return result
+    
+print(add(b=23, a=45))
+int(a)
+
+
--- guess_game.py
+++ guess_game.py
@@ -13,8 +13,19 @@ for i in range(1,6):
         break
     
     else: 
-        print("You did not guess correctly. you have", (5-i), "trials remaining")
+        print("You did not guess correctly. you have", (5-i), "trials remaining!")
+        # Provide a hint whether the random number is even or odd
+        if correct_number % 2 == 0:
+            print("Hint: The correct number is even.")
+        else:
+            print("Hint: The correct number is odd.")
+
+        # Provide a hint whether the random number is higher or lower than your guessed
+        if correct_number > num:
+            print("Hint2: The correct number is higher than your guessed number.")
+        else:
+            print("Hint2: The correct number is lower than your guessed number.")
 
 else:
-   print("You have exceeeded your maximum trial. please restart the Game")         
+   print("You have exceeded your maximum trial. please restart the Game")         
     
